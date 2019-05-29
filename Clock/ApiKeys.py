f6c62b673a44a1062c8ae57f7bc9b9f876c259a8
Josh Hiatt <43183677+jshiatt@users.noreply.github.com>
2019-05-29 12:42:05 -0400

    Add files via upload

---
+++ Clock/ApiKeys.py
@@ -0,0 +1,9 @@
+# change this to your API keys
+# DarkSky API key
+dsapi = 'e58b7b832b0edb1a0ab59e6af8473b7f'
+
+# Map API keys -- only needs 1 of the following
+# Google Maps API key (if usemapbox is not set in Config)
+googleapi = 'AIzaSyD5BHaZqMytirke4Z4Y6kVmik27kZq1yaI'
+# Mapbox API key (access_token) [if usemapbox is set in Config]
+mbapi = 'YOUR MAPBOX ACCESS TOKEN'
